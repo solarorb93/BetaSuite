c106980efee41c6100da7acc7a6b4e5019a689cd
solarorb93 <107260675+solarorb93@users.noreply.github.com>
2022-07-29 20:20:14 -0700

    Update betaconfig.py

--- betaconfig.py
+++ betaconfig.py
@@ -76,8 +76,8 @@ items_to_censor = [
 # censor style: whether the default censor should be black bars, pixelate, or blur.  Uncomment
 # one of the below (by removing the #).
 # you can override these per item below in Item Overrides
-default_censor_style = [ 'bar', (255,0,0) ] # second item is the color of the bar, in RGB code 
-#default_censor_style = [ 'bar', (247,154,192) ] # second item is the color of the bar, in RGB code 
+#default_censor_style = [ 'bar', (0,0,0) ] # second item is the color of the bar, in RGB code 
+default_censor_style = [ 'bar', (247,154,192) ] # second item is the color of the bar, in RGB code 
 #default_censor_style = [ 'blur', 50 ] # second item is how aggressive a blur.  20 is a reasonable number.  Higher is more blurry.
 #default_censor_style = [ 'pixel', 40 ] # second item is the how much to pixelate.  Higher is more censored.  10 means that a 200x400 pixel region is pixelated to 20x40 pixels.
 
